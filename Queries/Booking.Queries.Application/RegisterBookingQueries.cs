c5ddc00de13bf639c10049e9ed6556bad0b32655
Silvana <sil_s_s@yahoo.com.br>
2022-02-04 17:52:17 -0300

    Updated booking queries

--- Queries/Booking.Queries.Application/RegisterBookingQueries.cs
+++ Queries/Booking.Queries.Application/RegisterBookingQueries.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Booking.Infrastructure.CQRS.Queries;
+using Booking.Infrastructure.Storage.SqlServer;
+using Booking.Queries.Application.Hotel.Processors;
+using Booking.Queries.Application.Hotel.Query;
+using Booking.Queries.Application.Hotel.ReadModel;
+using Booking.Queries.Application.Repository;
+using Booking.Queries.Application.Reservation.Processors;
+using Booking.Queries.Application.Reservation.Query;
+using Booking.Queries.Application.Reservation.ReadModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Booking.Queries.Application
 {
-    public class RegisterBookingQueries
+    public static class RegisterBookingQueries
     {
+        public static IServiceCollection RegisterQueriesApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterSqlServerInfrastructureDependencies(configuration);
+
+            services.AddTransient<IQueryHandler<RetrieveReservationDetail, ReservationDetail>, RetrieveReservationDetailHandler>();
+            services.AddTransient<IQueryHandler<HotelQuery, IEnumerable<HotelListItem>>, HotelQueryHandler>();
+            services.AddTransient<IQueryHandler<AvailableRoomsQuery, IEnumerable<AvailableRooms>>, HotelQueryHandler>();
+            services.AddTransient<IQueryHandler<RoomQuery, IEnumerable<RoomListItem>>, HotelQueryHandler>();
+            services.AddTransient<IQueryHandler<CurrentAddressQuery, CurrentAddress>, HotelQueryHandler>();
+            services.AddTransient<IQueryHandler<CurrentContactsQuery, CurrentContacts>, HotelQueryHandler>();
+
+            services.AddSingleton<ReservationPersistence, ReservationPersistence>();
+            services.AddSingleton<HotelPersistence, HotelPersistence>();
+
+            return services;
+        }
+
     }
 }
