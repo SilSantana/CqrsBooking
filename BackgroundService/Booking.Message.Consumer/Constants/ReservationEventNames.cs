c651dd560785e46bbc2faf45dfb70dadaf22d7f6
Silvana <sil_s_s@yahoo.com.br>
2022-02-09 16:07:01 -0300

    Added Booking Message Consumer constants

---
+++ BackgroundService/Booking.Message.Consumer/Constants/ReservationEventNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.Message.Consumer.Constants
+{
+    public static class ReservationEventNames
+    {
+        public const string ReservationCreated = "ReservationCreated";
+        public const string ReservationCanceled = "ReservationCanceled";
+
+    }
+}
