1559a243e9eb3f10bb4f6a586bb79e60aa9909a5
Silvana <sil_s_s@yahoo.com.br>
2022-02-09 15:04:21 -0300

    Added Booking Api Read structure

---
+++ Api/Booking.ApiRead/Controllers/BookingController.cs
@@ -0,0 +1,45 @@
+﻿using Booking.Infrastructure.CQRS.Commands;
+using Booking.Infrastructure.CQRS.Queries;
+using Booking.Queries.Application.Reservation.Query;
+using Booking.Queries.Application.Reservation.ReadModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Booking.ApiRead.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+
+        private readonly IQueryProcessor _queryProcessor;
+
+        public BookingController(IQueryProcessor queryProcessor)
+        {
+            _queryProcessor = queryProcessor;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(CommandResult), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{reservationCode}/detail")]
+        [ProducesResponseType(typeof(CommandResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetReservationDetail(string reservationCode)
+        {
+            var retrieveReservationDetail = new RetrieveReservationDetail(reservationCode);
+
+            var result = await _queryProcessor.ExecuteQueryAsync<RetrieveReservationDetail, ReservationDetail>(retrieveReservationDetail);
+
+            return Ok(result);
+        }
+
+    }
+}
--- Api/Booking.ApiRead/Startup.cs
+++ Api/Booking.ApiRead/Startup.cs
@@ -1,14 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Booking.Infrastructure.CQRS;
+using Booking.Queries.Application;
+
 
 namespace Booking.ApiRead
 {
@@ -25,6 +22,21 @@ namespace Booking.ApiRead
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                             new Microsoft.OpenApi.Models.OpenApiInfo
+                             {
+                                 Title = "CqrsBooking Api",
+                                 Version = "v1",
+                                 Description = "List of Apis to interact with CqrsBooking"
+                             });
+            });
+
+            services
+                //.RegisterInfrastructureCqrsDependencies(Configuration)
+                .RegisterQueriesApplicationDependencies(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +54,14 @@ namespace Booking.ApiRead
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("./swagger/v1/swagger.json", "CqrsBooking Api V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
